37dc06ecfab9c2f7bfd546f1181be124aefe741f
bhumika biyani <73336200+bhumikabiyani@users.noreply.github.com>
2026-04-08 13:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const year = document.getElementById("year").value.trim();
+
+  const params = new URLSearchParams();
+
+  if (name) params.append("name", name);
+  if (year) params.append("year", year);
+
+  let baseURL = "https://localhost:8080/";
+  let finalURL = baseURL + (params.toString() ? "?" + params.toString() : "");
+
+  document.getElementById("url").textContent = finalURL;
+});
